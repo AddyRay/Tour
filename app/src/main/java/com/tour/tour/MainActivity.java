688f4ee9b118990ad969bdfaa3f5a1922064887a
Adrita Ray <Adrita Ray>
2017-10-07 11:23:13 +0530

    first commit

---
+++ app/src/main/java/com/tour/tour/MainActivity.java
@@ -0,0 +1,26 @@
+package com.tour.tour;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Signup(View view)
+    {
+        Intent i = new Intent(this,SignupActivity.class);
+        startActivity(i);
+    }
+}
